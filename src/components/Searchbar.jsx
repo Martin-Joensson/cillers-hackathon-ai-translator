9713cae6a19506b43a2f0a14653dc5a2066bfd53
Erica Mechler <erica.mechler1@gmail.com>
2024-06-08 17:08:19 +0200

    styling search-bar

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,10 +1,13 @@
+import "./Searchbar.css";
 import { FaMagnifyingGlass } from "react-icons/fa6";
 
 export const Searchbar = () => {
   return (
-    <label>
-      <FaMagnifyingGlass />
-      <input type="text" placeholder="Search language..." />
-    </label>
+    <div className="search-container">
+      <label>
+        <FaMagnifyingGlass className="search-icon" />
+        <input type="text" placeholder="Search language ..." />
+      </label>
+    </div>
   );
 };
